aeceed57921443c28844367a9138e1e2fd653c8d
raimundo <rfmeloneto@gmail.com>
2025-05-14 12:08:46 -0300

    add dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
    WidgetsFlutterBinding.ensureInitialized();
-   
+    // const supabaseUrl = 'https://ovywrdyprqmvaortudua.supabase.co';
+    // const supabaseKey = String.fromEnvironment('SUPABASE_KEY');
+    await dotenv.load(fileName: ".env");
+    await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_KEY']!, 
+  );
+
   runApp(const MainApp());
 }
+final supabase = Supabase.instance.client;
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
